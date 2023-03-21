c729533892650b9c9722bd731a8eb0b565ae4f21
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-21 21:47:04 +0530

    ADDED_UC2

--- src/com/LinkedListImplementation/LinkedList.java
+++ src/com/LinkedListImplementation/LinkedList.java
@@ -4,4 +4,23 @@ public class LinkedList<T> {
     Node<T> head;
     Node<T> tail;
 
+    public void add( T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void show() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 }
--- src/com/LinkedListImplementation/LinkedListMain.java
+++ src/com/LinkedListImplementation/LinkedListMain.java
@@ -1,7 +1,11 @@
 package com.LinkedListImplementation;
 
 public class LinkedListMain {
-
-    //UC1
-    LinkedList<Integer> linkedList1 = new LinkedList<>();
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.add(70);
+        linkedList1.add(30);
+        linkedList1.add(56);
+        linkedList1.show();
+    }
 }
