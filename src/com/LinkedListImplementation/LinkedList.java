5a3fde6c5538ba5ecaaa8a1be49e827ac8eba4d9
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-21 21:23:03 +0530

    ADDED

---
+++ src/com/LinkedListImplementation/LinkedList.java
@@ -0,0 +1,7 @@
+package com.LinkedListImplementation;
+
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+}
---
+++ src/com/LinkedListImplementation/LinkedListMain.java
@@ -0,0 +1,5 @@
+package com.LinkedListImplementation;
+
+public class LinkedListMain {
+    LinkedList<Integer> linkedList1 = new LinkedList<>();
+}
